9bef96bebd5e0ee1cc43534c1c4967275890834e
Ravi <samcollins9899@gmail.com>
2024-12-24 00:32:05 +0530

    added signin page

--- src/components/auth/sign-in-page.tsx
+++ src/components/auth/sign-in-page.tsx
@@ -1,10 +1,44 @@
-import { Card } from "../ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "../ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import { Separator } from "../ui/separator";
 
 const SignInPage = () => {
   return (
     <div>
       <Card className="w-[350px]">
-        <p>hello</p>
+        <CardHeader className="flex items-center justify-center">
+          <div className="flex items-center gap-2">
+            <img src="/svg/logo.svg" alt="logo" />
+            <CardTitle className="pt-3 ">CodeAnt AI</CardTitle>
+          </div>
+          <CardDescription>Welcome to CodeAnt AI</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Tabs defaultValue="account" className="">
+            <TabsList className="grid w-full grid-cols-2 rounded-lg">
+              <TabsTrigger className=" bg-[#2563eb] text-white" value="account">
+                SAAS
+              </TabsTrigger>
+              <TabsTrigger className="rounded-xl" value="password">
+                Self Hosted
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="account">
+              <button>Signin with github</button>
+            </TabsContent>
+            <TabsContent value="account">
+              <button>Signin with github</button>
+            </TabsContent>
+            <Separator className="my-2" />
+            <div>hhh</div>
+          </Tabs>
+        </CardContent>
       </Card>
     </div>
   );
